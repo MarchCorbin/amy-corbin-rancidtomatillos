179fd92f06476d98f499686beeb87fbf4a033948
Amy Karnaze <akarnaze@gmail.com>
2020-08-18 16:39:13 -0600

    Create basic header outline and associated files

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <header>
+        <h1>Perished Durians</h1>
+        <button>Log In</button>
+      </header>
+    )
+  }
+}
+
+export default Header
