df22d81e2a1d1cf8df1873aab1276176f1160e44
Amy Karnaze <akarnaze@gmail.com>
2020-08-18 17:01:43 -0600

    Add initial Movies component structure and associated files

---
+++ src/Components/Movies/Movies.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+class Movies extends Component {
+  constructor() {
+    super()
+   
+  }
+
+  render() {
+    return (
+      <section class="movies-container">
+        
+      </section>
+      
+    ) 
+
+  }
+
+  export default Movies;
