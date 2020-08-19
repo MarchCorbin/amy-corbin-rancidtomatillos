3c091fe942e1a16b693eb6640790dc6f27971c08
Amy Karnaze <akarnaze@gmail.com>
2020-08-18 20:33:49 -0600

    Add mapped movie info to render

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import './Card.scss'
+
 
 function Card({allMovies}) {
   let eachMovie;
@@ -9,15 +11,15 @@ function Card({allMovies}) {
     eachMovie = allMovies.map(movie => {
     console.log('here', movie)  
     return (
-      <div>
+      <div className="movie-card">
+        <p>{movie.title}</p>
         <img src={movie.poster_path}></img>
+        <p>{movie.average_rating}</p>
+        <p>{movie.release_date}</p>
       </div>
     )
   })
-    
       }
-  
-    // }
     return <h1>{eachMovie}</h1>
 }
 
