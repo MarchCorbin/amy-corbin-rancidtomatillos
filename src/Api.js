82d990f28509f6509601f8dc47d5fe4960e5b1ac
Amy Karnaze <akarnaze@gmail.com>
2020-08-18 20:07:46 -0600

    Get movies from api

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,10 @@
 export const fetchAllMovies = () => {
-  return fetch()
+  return fetch("https:rancid-tomatillos.herokuapp.com/api/v2/movies")
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw new Error("error, please try again")
+    }
+  })
 }
