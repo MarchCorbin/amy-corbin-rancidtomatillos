d6b33d32200991c1b4f392fa3ea7046494359c95
Amy Karnaze <akarnaze@gmail.com>
2020-08-18 20:10:12 -0600

    Add fetch and set state and render movies

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
-
+import { fetchAllMovies } from '../../Api'
+import Card from '../Card/Card'
 
 class Movies extends Component {
   constructor() {
     super()
-   
+    this.state = {
+      allMovies: []
+    }
+  }
+
+  componentDidMount() {
+    fetchAllMovies()
+    .then(data => {this.setMovieState(data.movies)})
+    .catch(error => console.log(error))
+  }
+
+  setMovieState(data) {
+    this.setState({allMovies: data})
+    // console.log("gotit", this.state.allMovies)
   }
 
   render() {
     return (
       <section class="movies-container">
-        
+        <Card allMovies= {this.state.allMovies}/>
       </section>
-      
     ) 
-
   }
+}
 
   export default Movies;
