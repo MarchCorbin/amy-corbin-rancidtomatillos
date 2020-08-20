67b931600d3f635a6b337ba9ceb1f8f53a7ad74e
Amy Karnaze <akarnaze@gmail.com>
2020-08-19 20:39:58 -0600

    Add props to access current user in header and some pseudocode

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,10 +3,11 @@ import './Header.scss'
 import { Link } from 'react-router-dom'
 
 class Header extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    console.log(this.props, 'props')
   }
-
+  
   render() {
     return (
       <header className="header">
@@ -18,3 +19,8 @@ class Header extends Component {
 }
 
 export default Header
+
+// think of brwoser history as an array
+//adding things to array when click link
+// using rrd to change path
+// console.log 
