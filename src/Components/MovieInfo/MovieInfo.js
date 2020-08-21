1dbf7fbb2268fbd2cb5faa124b9d793db7592a24
Amy Karnaze <akarnaze@gmail.com>
2020-08-20 18:12:18 -0600

    Add movieInfo class

---
+++ src/Components/MovieInfo/MovieInfo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './MovieInfo.scss'
+
+
+class MovieInfo extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+
+  }
+
+
+  render() {
+    return <h1>MMMMM</h1>
+  } 
+}
+
+export default MovieInfo
