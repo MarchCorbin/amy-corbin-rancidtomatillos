e9f2017e042d85e57c3ade935638cad97a7f27b7
Amy Karnaze <akarnaze@gmail.com>
2020-08-20 18:12:46 -0600

    Display individual movies in Movies

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -21,10 +21,43 @@ class Movies extends Component {
     this.setState({allMovies: data})
   }
 
+  displayAllMovies() {
+    if (this.state.allMovies.length) {
+      return this.state.allMovies.map(movie => {
+        return <Card
+          movieTitle={movie.title}
+          movieId={movie.id}
+          moviePoster={movie.poster_path}
+          movieRating={movie.average_rating}
+          releaseDate={movie.release_date}
+          backdrop={movie.backdrop_path}
+        />
+      })
+    }
+  }
+
+  // function displayAllMovies() {
+  //   if (allMovies.length) {
+  //   return allMovies.map(movie => {
+  //   return (
+  //     <div className="movie-card">
+  //       {movie.title.length > 25 ? 
+  //         <p className='movie-titles'>{movie.title.slice(0, 18)}...</p> : 
+  //         <p className='movie-titles'>{movie.title}</p> }
+  //       <img id={movie.id} src={movie.poster_path} alt={movie.title}></img>
+  //       <p className="card-text">Avg Rating: {movie.average_rating}</p>
+  //       <p className="card-text">Release Date: {movie.release_date}</p>
+  //     </div>
+  //   )
+  //   })
+  //     }
+  //   }
+  //   return <div className="movies-section">{displayAllMovies()}</div>
+
   render() {
     return (
-      <section class="movies-container">
-        <Card allMovies= {this.state.allMovies}/>
+      <section className="movies-container">
+        {this.displayAllMovies()}
       </section>
     ) 
   }
