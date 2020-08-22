8916e0fcb01065eec07dfaa60cf5a47bb5494050
Amy Karnaze <akarnaze@gmail.com>
2020-08-21 19:08:26 -0600

    Add initial test structure

---
+++ src/Components/MovieInfo/MovieInfo.test.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { screen, fireEvent, render } from '@testing-library/react';
+// import '@testing-library/jest-dom';
+
+// describe('MovieInfo Component', () => {
+//   it('should have the correct content when rendered', () => {
+
+
+//   })
+
+// })
