3474dad938f7a7d125605156d494131d1f705405
Amy Karnaze <akarnaze@gmail.com>
2020-08-25 17:37:36 -0600

    api test attempt  draft

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,41 @@
-// import React from 'react';
-// import { screen, fireEvent, render } from '@testing-library/react';
-// import '@testing-library/jest-dom';
-// import App from './App';
-// import Card from './Components/Card/Card';
+import React from 'react';
+import '@testing-library/jest-dom';
+import App from './App';
+import Card from './Components/Card/Card';
+import { screen, fireEvent, render, waitFor } from '@testing-library/react;
+import { fetchUserMovieRatings } from './Api';
+jest.mock('.../Api')
+import MutationObserver from '@sheerun/mutationobserver-shim';
+window.MutationObserver = MutationObserver;
+import movieData from './movieData'
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
 // describe('App Component', () => {
-//   it('should have the correct content when rendered', () => {
+//   it('should display all movies', () => {
+        
 
-//     render(<Card
-//       )
+//     render(<App />)
+
+//     const containerHeading = screen.getByRole('heading', {name: ??})
+//  )
 //   })
 
 // })
+
+// describe(App, () => {
+//   it('it should render all cards', () => {
+
+
+
+//   })
+// })
+
+// test headers
+// test ratings
+// test how many cards get rendered to page
