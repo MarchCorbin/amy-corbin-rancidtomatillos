8bbfbe8514e11a7850bd9358a44fed4f2a64e411
Amy Karnaze <akarnaze@gmail.com>
2020-08-25 17:37:12 -0600

    Add mock movie data file

---
+++ src/movieData.js
@@ -0,0 +1,32 @@
+export const movieData = [
+  {
+    "id": 524047,
+    "poster_path": "https://image.tmdb.org/t/p/original//sA154deR0X51EcR2lm2FfDczryg.jpg",
+    "backdrop_path": "https://image.tmdb.org/t/p/original//juzEhsX92if2lJ2CSqKAI4RQswt.jpg",
+    "title": "Greenland",
+    "average_rating": 6.571428571428571,
+    "release_date": "2020-07-29"
+  }, {
+    "id": 606234,
+    "poster_path": "https://image.tmdb.org/t/p/original//eDnHgozW8vfOaLHzfpHluf1GZCW.jpg",
+    "backdrop_path": "https://image.tmdb.org/t/p/original//u9YEh2xVAPVTKoaMNlB5tH6pXkm.jpg",
+    "title": "Archive",
+    "average_rating": 7.6,
+    "release_date": "2020-08-13"
+  }, {
+    "id": 149,
+    "poster_path": "https://image.tmdb.org/t/p/original//5KlRFKKSbyCiyYpZSS3A6G5bW0K.jpg",
+    "backdrop_path": "https://image.tmdb.org/t/p/original//qZ4NYuwME0j6QgJmIE6AZMgmCaj.jpg",
+    "title": "Akira",
+    "average_rating": 6,
+    "release_date": "1988-07-16"
+  }, {
+    "id": 437518,
+    "poster_path": "https://image.tmdb.org/t/p/original//puVA7UoLuu7Q7OtGRWINhT2XpOB.jpg",
+    "backdrop_path": "https://image.tmdb.org/t/p/original//z8PntmDQqNt8ertwoXQTFoMQg5u.jpg",
+    "title": "Friend of the World",
+    "average_rating": 3.3333333333333335,
+    "release_date": "2020-08-15"
+  }
+
+]
