3d2eaaff86231e06550473ad01430e45f73e7a15
Amy Karnaze <akarnaze@gmail.com>
2020-08-30 21:43:38 -0600

    Add post for comments

--- src/Api.js
+++ src/Api.js
@@ -13,6 +13,7 @@ return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieId}`)
 .then(res => res.json())
 }
 
+
 export const fetchUserMovieRatings = (id) => {
 return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/users/${id}/ratings`)
 .then(res => res.json())
@@ -41,7 +42,6 @@ export const postLogin = (email, password) => {
 
 export const postUserRating = async (userId, id, rating) => {
   let movRating = parseInt(rating)
-  console.log("nn", movRating)
   return await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/users/${userId}/ratings`,
   {
     method: 'POST',
@@ -53,7 +53,6 @@ export const postUserRating = async (userId, id, rating) => {
   })
   })
   .then(res => res.json())
-  .then(data => console.log(data))
   .catch(err => console.log(err))
 
 }
@@ -69,6 +68,25 @@ export const deleteSingleRating = async(userId, ratingId) => {
   .catch(err => alert(err.message));
 }
 
+export const postComment = async (movieId, author, comment) => {
+  return await fetch(`http://localhost:3001/api/v1/movies/${movieId}/comments`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "author": author,
+      "comment": comment
+    }),
+  })
+  .then(res => {
+    if (res.ok) {
+      return res.json()
+    } else {
+      throw alert('Invalid comment, Please try again')
+    }
+  })
+}
 // export const deleteFromFavorites = async (id) => {
 //   return await fetch(`http://localhost:3001/api/v1/favorites/${id}`, {
 //       method: 'DELETE'
