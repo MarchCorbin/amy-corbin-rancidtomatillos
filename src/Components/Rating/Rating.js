376060552b80b18ed5b7350fa32e61cadc45c427
Amy Karnaze <akarnaze@gmail.com>
2020-08-31 15:47:01 -0600

    remove uneed componentDidMount, add proptypes, fix spacing

--- src/Components/Rating/Rating.js
+++ src/Components/Rating/Rating.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import StarRatingComponent from 'react-star-rating-component';
 import './Rating.scss'
 import { postUserRating } from '../../Api'
+import PropTypes from 'prop-types'
 
- 
 class Rating extends React.Component {
   constructor(props) {
     super(props);
@@ -13,24 +13,18 @@ class Rating extends React.Component {
       hasRated: false
     };
   }
- 
-  componentDidMount = () => {
-
-  }
 
   onStarClick = async(nextValue, prevValue, name) => {
       let id = this.props.movieId
       let userId = this.props.userId
       this.setState({rating: nextValue});
-
       await postUserRating(userId, id, nextValue)
       this.props.updateRating(nextValue)
-       this.props.getUserMovieRatings()
+      this.props.getUserMovieRatings()
   }
- 
+
   render() {
     const { rating } = this.state;
-    
     return (                
       <div className="star-container">
         <h2 className='descrip-text rating-text'>Rate This Movie!</h2>
@@ -46,3 +40,10 @@ class Rating extends React.Component {
 }
 
 export default Rating;
+
+Rating.propTypes = {
+  updateRating: PropTypes.func.isRequired,
+  getUserMovieRatings: PropTypes.func.isRequired,
+  movieId: PropTypes.number.isRequired,
+  userId: PropTypes.number.isRequired
+}
