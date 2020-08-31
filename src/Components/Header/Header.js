a1779aacf424e99a2999e2a53141f7de314a2b1f
Amy Karnaze <akarnaze@gmail.com>
2020-08-31 15:49:04 -0600

    add proptypes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Header.scss'
 import { Link } from 'react-router-dom'
 import durian from "../../Assets/durianlogo.png"
+import PropTypes from 'prop-types'
 
 class Header extends Component {
   constructor(props) {
@@ -9,8 +10,6 @@ class Header extends Component {
     this.state = {
     }
   }
-
-  
 
   render() {
     return (
