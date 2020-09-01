1cbafc75f4457612bc350cff0d369cd383847162
Amy Karnaze <akarnaze@gmail.com>
2020-09-01 17:33:27 -0600

    only render form when user is logged in

--- src/Components/CommentForm/CommentForm.js
+++ src/Components/CommentForm/CommentForm.js
@@ -9,35 +9,43 @@ class CommentForm extends Component {
     this.state = {
       author: '',
       comment: '',
-      movieId: this.props.movieId
+      movieId: this.props.movieId,
+      isValid: false
     }
   }
 
   changeHandler = (event) => {
     const inputName = event.target.name;
     this.setState({[inputName]: event.target.value})
+    if (event.target.value.length >= 1) {
+      this.setState({isValid: true})
+    } else if(event.target.value === 0) {
+      this.setState({isValid: false})
+    }
   }
 
   submitComment = (e) => {
     e.preventDefault();
+      if (this.state.isValid === true) {
     const movieId = this.props.movieId;
     const authorInput = this.state.author
     const commentInput = this.state.comment
     postComment(movieId, authorInput, commentInput)
     this.props.addComment(commentInput, authorInput)
     this.clearInputs()
+     }
   }
 
   clearInputs = () => {
-    this.setState({author: '', comment: ''})
+    this.setState({author: '', comment: '', isValid: false})
   }
 
   render() {
     return (
-      <section className="comments-container">
-        <h2>Comments section</h2>
-        <input className="author-comment" type="text" name="author"  aria-label="author-comment" placeholder="name" onChange={this.changeHandler}value={this.state.author}/>
-        <input type="text" name="comment" className="new-comment" aria-label="new-comment" placeholder="comment" onChange={this.changeHandler}value={this.state.comment}/>
+      <section className="comments-container" id="comments-form">
+        <h2>Comments</h2>
+        <input className="author-comment" type="text" name="author"  aria-label="author-comment" placeholder="name" onChange={this.changeHandler} value={this.state.author}></input>
+        <input type="text" name="comment" className="new-comment" aria-label="new-comment" placeholder="comment" onChange={this.changeHandler} value={this.state.comment}></input>
         <button className="comment-button" onClick={this.submitComment}>New Comment</button>
       </section>
     )
