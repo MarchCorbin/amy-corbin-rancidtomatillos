a1007d6f12c13d8718dc875211429a54f1b46129
Corbin March <marchcorbin@gmail.com>
2020-09-01 18:13:54 -0600

    Fixed personal ratings bug

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ addToFavorites = async(movieId) => {
           </Route>
           <Route exact path='/'>
             <Header toggleFavButton={this.toggleFavButton()} changingMessage={personalizedMessage} toggleButton={this.toggleButton()} />
-            <Movies userRatings={this.state.userRatings} />
+            <Movies isLoggedIn={this.state.isLoggedIn} userRatings={this.state.userRatings} />
           </Route>
         </BrowserRouter>
       </main>
