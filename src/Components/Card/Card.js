e3b257808886d0cff6255745dd04cf4bf9deedb2
Amy Karnaze <akarnaze@gmail.com>
2020-09-01 18:51:27 -0600

    Reduce title length

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import './Card.scss'
 import { Link } from 'react-router-dom'
-import { fetchSingleMovieDetails } from '../../Api.js'
-import MovieInfo from '../MovieInfo/MovieInfo'
+
 import PropTypes from 'prop-types'
 
 function Card({movieTitle, movieId, moviePoster, movieRating, releaseDate, backdrop, userRating, isLoggedIn}) {
   return (
     <div className = "movie-card" placeholder="mov-card">
-   {movieTitle.length > 25 ? <p className='movie-titles'>{movieTitle.slice(0, 18)}...</p> : <p className='movie-titles'>{movieTitle}</p> }
+   {movieTitle.length > 15 ? <p className='movie-titles'>{movieTitle.slice(0, 15)}...</p> : <p className='movie-titles'>{movieTitle}</p> }
    <Link to={{
      pathname:`/movies/${movieId}`
     }}>
